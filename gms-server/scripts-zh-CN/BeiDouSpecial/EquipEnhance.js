cb8792d928b5f5772049a334f454e8deca619ddb
wudaown <wudaown@gmail.com>
2026-03-02 08:55:55 +0000

    Rewrite script without Java changes for UI

--- gms-server/scripts-zh-CN/BeiDouSpecial/EquipEnhance.js
+++ gms-server/scripts-zh-CN/BeiDouSpecial/EquipEnhance.js
@@ -14,10 +14,11 @@ const DEF_BONUS = 100;
 const HPMP_BONUS = 100;
 
 var status = 0;
+var itemId = 0;
 
 function start() {
     status = 0;
-    cm.sendSimple(cm.getEnhanceInfo());
+    cm.sendSimple(getMenuText());
 }
 
 function action(mode, type, selection) {
@@ -30,8 +31,15 @@ function action(mode, type, selection) {
     
     if (status === 1) {
         if (selection === 0) {
-            // 确认强化
-            cm.sendYesNo(cm.getEnhanceConfirm());
+            // 检查条件
+            let check = checkConditions();
+            if (check !== "") {
+                cm.sendOk(check);
+                cm.dispose();
+                return;
+            }
+            itemId = cm.getFirstInventoryEquipItemId();
+            cm.sendYesNo(getConfirmText());
         }
     } else if (status === 2) {
         // 执行强化
@@ -39,31 +47,47 @@ function action(mode, type, selection) {
     }
 }
 
-function doEnhance() {
+function getMenuText() {
+    let text = "#e#d装备强化系统#k#n\r\n\r\n";
+    text += "消耗物品:\r\n";
+    text += "  #i" + MAPLE_LEAF + "##t" + MAPLE_LEAF + "# x " + COST_MAPLE_LEAF + " 个\r\n";
+    text += "  金币 x " + COST_MESO + "\r\n\r\n";
+    text += "强化效果:\r\n";
+    text += "  力量 +100 | 敏捷 +100\r\n";
+    text += "  智力 +100 | 运气 +100\r\n";
+    text += "  物理攻击 +100 | 魔法攻击 +100\r\n";
+    text += "  物理防御 +100 | 魔法防御 +100\r\n";
+    text += "  HP +100 | MP +100 | 装备等级+1\r\n\r\n";
+    text += "#L0# #r开始强化#k#l\r\n";
+    return text;
+}
+
+function getConfirmText() {
+    return "#e#d确认强化#k#n\r\n\r\n"
+        + "即将强化以下装备:\r\n"
+        + "  #i" + itemId + "##t" + itemId + "#\r\n\r\n"
+        + "强化后装备属性将提升，装备等级+1\r\n"
+        + "原有装备将被删除，是否确认？";
+}
+
+function checkConditions() {
     if (cm.getItemQuantity(MAPLE_LEAF) < COST_MAPLE_LEAF) {
-        cm.sendOk("枫叶不足，需要 " + COST_MAPLE_LEAF + " 个枫叶");
-        cm.dispose();
-        return;
+        return "枫叶不足，需要 " + COST_MAPLE_LEAF + " 个枫叶";
     }
     if (cm.getMeso() < COST_MESO) {
-        cm.sendOk("金币不足，需要 " + COST_MESO + " 金币");
-        cm.dispose();
-        return;
+        return "金币不足，需要 " + COST_MESO + " 金币";
     }
-    
-    let oldItemId = cm.getFirstInventoryEquipItemId();
-    if (oldItemId === 0) {
-        cm.sendOk("背包装备栏没有装备");
-        cm.dispose();
-        return;
+    let eqId = cm.getFirstInventoryEquipItemId();
+    if (eqId === 0) {
+        return "背包装备栏没有装备";
     }
-    
-    if (!cm.canHold(oldItemId, 1)) {
-        cm.sendOk("背包空间不足，无法放入装备");
-        cm.dispose();
-        return;
+    if (!cm.canHold(eqId, 1)) {
+        return "背包空间不足，无法放入装备";
     }
-    
+    return "";
+}
+
+function doEnhance() {
     cm.gainItem(MAPLE_LEAF, -COST_MAPLE_LEAF);
     cm.gainMeso(-COST_MESO);
     
@@ -74,6 +98,6 @@ function doEnhance() {
         HPMP_BONUS, HPMP_BONUS
     );
     
-    cm.sendOk("强化成功！\r\n\r\n原始装备ID: #i" + oldItemId + "##t" + oldItemId + "#\r\n强化属性: 力量+100 敏捷+100 智力+100 运气+100 物理攻击+100 魔法攻击+100 物理防御+100 魔法防御+100 HP+100 MP+100");
+    cm.sendOk("强化成功！\r\n\r\n装备已强化完成");
     cm.dispose();
 }
